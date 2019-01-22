ac12030225ea8b3e133a2992e540d67d1bc3a09a
shun <shun@getto.systems>
2019-01-22 09:51:20 +0000

    refactor: Menu

--- modules/layout/getto/src/GettoUpload/Layout/Menu/Model.elm
+++ modules/layout/getto/src/GettoUpload/Layout/Menu/Model.elm
@@ -1,14 +1,28 @@
 module GettoUpload.Layout.Menu.Model exposing
   ( Menu
-  , Item(..)
+  , Item
+  , Info
   , item
+  , info
+  , children
   )
 import GettoUpload.Layout.Fa as Fa
 
 type alias Menu = List ( String, List Item )
 
 type Item
-  = Item Fa.Icon String (List Item)
+  = Item Info (List Item)
+
+type alias Info =
+  { icon : Fa.Icon
+  , href : String
+  }
 
 item : Fa.Icon -> String -> List Item -> Item
-item = Item
+item icon href items = Item { icon = icon, href = href } items
+
+info : Item -> Info
+info (Item entry _) = entry
+
+children : Item -> List Item
+children (Item _ items) = items
