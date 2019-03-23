00df8a2a3382a518137e4aa1a3e9df43d26f6825
shun <shun@getto.systems>
2019-03-23 06:05:27 +0000

    fix: all unused import check comment

--- bin/check-all-unused-import.sh
+++ bin/check-all-unused-import.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-[ -z "$(find modules -name "*.elm" | xargs -n 1 bin/check-unused-import.sh)" ]
+check_all_unused_import_main(){
+  local unused
+  unused=$(find modules -name "*.elm" | xargs -n 1 bin/check-unused-import.sh)
+  if [ -z "$unused" ]; then
+    echo "un-used import check: OK"
+  else
+    echo "un-used import check: NG"
+    echo $unused
+    exit 1
+  fi
+}
+
+check_all_unused_import_main "$@"
